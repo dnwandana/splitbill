931f08edea0de718fecc0ed31d61b891072bbfd1
Dana Wandana <dn.wandana@gmail.com>
2025-08-03 14:10:08 +0700

    add serverDir configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   srcDir: 'src',
+  serverDir: 'src/server',
   modules: ['@nuxt/eslint', '@nuxt/ui'],
 
   // nuxt ui
