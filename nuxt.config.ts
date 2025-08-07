e9071861b20e2f086ddb2dec50896fab80c8496e
Dana Wandana <dn.wandana@gmail.com>
2025-08-07 13:36:40 +0700

    config: disable server-side rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false,
   srcDir: 'src',
   serverDir: 'src/server',
   modules: ['@nuxt/eslint', '@nuxt/ui'],
